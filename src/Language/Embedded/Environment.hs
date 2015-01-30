b4483693bc4bf49fe7a61644ea8c75aed6e502a1
Emil Axelsson <emax@chalmers.se>
2015-01-30 16:25:19 +0100

    Added abstract type for environments with possibility of duplicates

--- src/Language/Embedded/Environment.hs
+++ src/Language/Embedded/Environment.hs
@@ -4,11 +4,16 @@ module Language.Embedded.Environment where
 
 
 
+import Data.List as List
 import Data.Map as Map
 
 import Data.Syntactic.Functional (Name (..))
 
 
+
+----------------------------------------------------------------------------------------------------
+-- * No duplicates
+----------------------------------------------------------------------------------------------------
 
 -- | Environment with information about variables in scope
 type Env a = Map Name a
@@ -23,7 +28,7 @@ singleEnv = Map.singleton
 
 -- | Insert a mapping into an environment
 insEnv :: Name -> a -> Env a -> Env a
-insEnv = insert
+insEnv = Map.insert
 
 -- | Insert a mapping into an environment
 (|>) :: (Name,a) -> Env a -> Env a
@@ -50,4 +55,51 @@ toListEnv = Map.toList
 -- If the same name appears several times, the last one has precedence.
 fromListEnv :: [(Name, a)] -> Env a
 fromListEnv = Map.fromList
+
+
+
+----------------------------------------------------------------------------------------------------
+-- * Duplicates
+----------------------------------------------------------------------------------------------------
+
+-- | Environment with potential duplicates
+newtype ENV a = ENV {toListENV :: [(Name,a)]}
+  deriving (Eq, Ord, Show)
+
+-- | Empty environment
+emptyENV :: ENV a
+emptyENV = ENV []
+
+-- | Environment with a single mapping
+singleENV :: Name -> a -> ENV a
+singleENV v a = ENV [(v,a)]
+
+-- | Insert a mapping into an environment. For functions like 'lookENV', the new name shadows any
+-- existing occurrences of the same name.
+insENV :: Name -> a -> ENV a -> ENV a
+insENV v a (ENV env) = ENV ((v,a) : env)
+
+-- | Insert a mapping into an environment. Newly inserted names shadow old names.
+(||>) :: (Name,a) -> ENV a -> ENV a
+def ||> ENV env = ENV (def:env)
+
+-- | Lookup a name in an environment
+lookENV :: Name -> ENV a -> Maybe a
+lookENV v = List.lookup v . toListENV
+
+-- | Like 'lookENV' but failing with the given message in case the name is not found
+lookENVNote :: String -> Name -> ENV a -> a
+lookENVNote msg v env = case lookENV v env of
+    Nothing -> error msg
+    Just a -> a
+
+-- | Delete all occurrences of a name from a environment
+delENV :: Name -> ENV a -> ENV a
+delENV v (ENV env) = ENV [def | def <- env, fst def /= v]
+
+-- | Lookup a name in an environment; return its definition and the rest of the environment
+splitENV :: Name -> ENV a -> Maybe (a, ENV a)
+splitENV v (ENV env) = case dropWhile ((v/=) . fst) env of
+    def:env' -> Just (snd def, ENV env')
+    _ -> Nothing
 
